89effd6c86e2afcc13ca8eec8c2f26d20d2780ff
CodeDevvv <vijaypatil0516@gmail.com>
2025-03-25 10:57:22 +0530

    Update

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose, { mongo } from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://root:admin@cluster0.g1z21.mongodb.net/QuickBite').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGO_DBurl).then(()=>console.log("DB Connected"));
 }
 
